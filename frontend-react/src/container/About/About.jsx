cb53d8cafa3acb287cc8f092ad1f596f8df60923
karnowsky1 <kestime9015@bths.edu>
2024-06-03 03:46:18 -0400

    completed work section on the front end. Created pills that can filter projects based on specific categories. User is now able to see project and hover over them to be given linkes to github repo & demo/README

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect } from 'react';
 import { motion } from 'framer-motion'
+import { AppWrap } from '../../wrapper';
 import './About.scss';
 import { urlFor, client } from '../../client'
 
@@ -44,4 +45,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
